d0a1a6f21472ce250986a40aa06b4fb35a696f30
Ackerr <wzmmmmj@gmail.com>
2022-10-26 17:34:29 +0800

    generate grpc gateway api

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/ackerr/grpc-gateway-example
+
+go 1.16
+
+require (
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.12.0
+	google.golang.org/genproto v0.0.0-20221014213838-99cd37c6964a
+	google.golang.org/grpc v1.50.1
+	google.golang.org/protobuf v1.28.1
+)
---
+++ rpc/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+
+	pb "github.com/ackerr/grpc-gateway-example/gen/proto"
+)
+
+type ExampleServer struct {
+	pb.UnimplementedExampleServiceServer
+}
+
+func (*ExampleServer) Echo(ctx context.Context, req *pb.ExampleRequest) (*pb.ExampleResponse, error) {
+	return &pb.ExampleResponse{Value: req.Key}, nil
+}
+
+var (
+	port = flag.Int("port", 8080, "The server port")
+)
+
+func main() {
+	flag.Parse()
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterExampleServiceServer(s, &ExampleServer{})
+	log.Printf("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
